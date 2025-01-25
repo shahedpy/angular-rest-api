13d4e910740bfcb0caa232fa2f13572a82254df3
Shahed <shahed_hridoy@outlook.com>
2025-01-25 15:47:08 +0600

    posts component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PostsComponent } from './posts/posts.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    PostsComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
