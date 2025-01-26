6b72636ecc08b7fa90a7d9aa8d7342ce2522fad7
Shahed <shahed_hridoy@outlook.com>
2025-01-26 12:04:00 +0600

    material added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { PostsComponent } from './posts/posts.component';
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet,
     PostsComponent
   ],
   templateUrl: './app.component.html',
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+
+interface Product {
+  id: number;
+  title: string;
+  description: string;
+}
+
 
 @Component({
   selector: 'app-posts',
-  imports: [],
+  imports: [
+    CommonModule,
+  ],
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.css'
 })
+
 export class PostsComponent {
 
+  products: Product[] = [];
+
+  constructor(private httpClient: HttpClient) { }
+
+  getProducts() {
+    this.httpClient.get<any>('https://dummyjson.com/products')
+      .subscribe((result) => {
+        this.products = result.products;
+      });
+  }
+
+  ngOnInit() {
+    this.getProducts();
+  }
 }
